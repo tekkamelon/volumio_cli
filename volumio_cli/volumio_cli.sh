8cb5eee31da510a6abaea96137c331c227ff13f8
tekkamelon <type2guntank@gmail.com>
2021-05-03 22:52:59 +0900

    プレイリストの表示,readの分岐
    プレイリストを改行して表示,readの分岐がうまく行かない

--- volumio_cli/volumio_cli.sh
+++ volumio_cli/volumio_cli.sh
@@ -3,12 +3,14 @@
 echo "http://<<please type your hostname>>.local"
 read "hostname"
 
-curl http://$hostname.local/api/v1/listplaylists
+curl http://$hostname.local/api/v1/listplaylists | sed 's/,/\n/g'
 
-echo "\nplease type command"
+# echo "\nplease type command" 
 echo "playback -> play"
 echo "pause -> pause"
 echo "stop -> stop"
-read "command"
-
-curl http://$hostname$http.local/api/v1/commands/?cmd=$command
+read -p "please type command" ; "command"
+	case "$command" in
+		[qQ]) echo "exit" ;;
+		*) curl http://$hostname$http.local/api/v1/commands/?cmd=$command
+	esac
