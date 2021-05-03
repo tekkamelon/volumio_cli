bce271cc38b43a0253266c47db909aeefe9fc114
tekkamelon <type2guntank@gmail.com>
2021-05-04 02:40:41 +0900

    利便性の向上
    ホスト名入力後に疎通確認,ホスト名の再設定コマンドの追加など

--- volumio_cli/volumio_cli.sh
+++ volumio_cli/volumio_cli.sh
@@ -1,42 +1,57 @@
 #!/bin/bash
 
-echo "http://<<hostname>>.local"
-read -p "hostname? " "hostname"
-# 入力されたホスト名を保存
-echo "$hostname" > /tmp/hostname &&
+# ホスト名の設定
+if ! cat /tmp/hostname ; then
+	echo "http://<<hostname>>.local" && read -p "hostname? > " "hostname" ; echo "$hostname" > /tmp/hostname
+else
+	:
+fi
 
-# プレイリスト一覧を表示,sedで改行,echoで空白行の挿入
-curl http://$(cat /tmp/hostname).local/api/v1/listplaylists | sed 's/,/\n/g' && echo -e "\n" 
+# pingで疎通確認,成功時のみ入力を待つ
+if ping -c 3 $(cat /tmp/hostname).local | grep ttl ; then
+	# プレイリスト一覧を表示,sedで改行,echoで空白行の挿入
+	curl http://$(cat /tmp/hostname).local/api/v1/listplaylists | sed 's/,/\n/g' && echo -e "\n" 
 
-echo "playback -> [p]"
-echo "pause -> [P]"
-echo "stop  -> [s]"
-echo "exit  -> [q]"
+# コマンド一覧
+echo "command list" 
+echo "	playback          -> [1]"
+echo "	pause             -> [2]"
+echo "	stop              -> [3]"
+echo "	change host       -> [C]"
+echo "	exit              -> [Q]"
 echo -n -e "\n"
 
-# "q"キーを入力で終了,それ以外で一覧に表示されたコマンドを入力で実行
+# "shift+q"キーを入力で終了,それ以外で一覧に表示されたコマンドを入力で実行
 while :
 do
 	read -p "command? > " "command"
 		case "$command" in
 			# 再生
-			[p])
-			curl http://$(cat /tmp/hostname).local/api/v1/commands/?cmd=play
+			[1])
+				curl http://$(cat /tmp/hostname).local/api/v1/commands/?cmd=play && echo -n -e "\n"
 			;;
 	
 			# 一時停止
-			[P])
-			curl http://$(cat /tmp/hostname).local/api/v1/commands/?cmd=pause
+			[2])
+				curl http://$(cat /tmp/hostname).local/api/v1/commands/?cmd=pause && echo -n -e "\n"
 			;;
 			
 			# 停止
-			[s])
-			curl http://$(cat /tmp/hostname).local/api/v1/commands/?cmd=stop
+			[3])
+				curl http://$(cat /tmp/hostname).local/api/v1/commands/?cmd=stop && echo -n -e "\n"
 			;;
 	
+			# ホスト名の再設定
+			[C])
+				echo "http://<<hostname>>.local" && read -p "hostname? > " "hostname" ; echo "$hostname" > /tmp/hostname ; exit 0
+			;;
+
 			# 終了
-			[q])
-			exit 0
+			[Q])
+				exit 0
 			;;
 		esac
 done
+else
+	rm /tmp/hostname
+fi
