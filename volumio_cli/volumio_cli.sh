713b0e964de85dc449607e9aafee6fcc19f6ee33
tekkamelon <type2guntank@gmail.com>
2021-05-04 01:03:37 +0900

    while文の追加,軽微な修正
    whileを使い入力の分岐,終了及び視認性の向上,コメントの追加

--- volumio_cli/volumio_cli.sh
+++ volumio_cli/volumio_cli.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
-echo "http://<<please type your hostname>>.local"
-read "hostname"
+echo "http://<<hostname>>.local"
+read -p "hostname? " "hostname"
+# 入力されたホスト名を保存
 echo "$hostname" > /tmp/hostname &&
 
-# プレイリスト一覧を表示,sedで改行
-curl http://$(cat /tmp/hostname).local/api/v1/listplaylists | sed 's/,/\n/g'
+# プレイリスト一覧を表示,sedで改行,echoで空白行の挿入
+curl http://$(cat /tmp/hostname).local/api/v1/listplaylists | sed 's/,/\n/g' && echo -e "\n" 
 
 echo "playback -> [p]"
 echo "pause -> [P]"
-echo "stop -> stop"
+echo "stop  -> [s]"
+echo "exit  -> [q]"
+echo -n -e "\n"
 
-# "q"キーを入力で終了,一覧に表示されたコマンドを入力で実行
-echo -n "command? > "
-read "command"
-	case "$command" in
-		[p])
-		curl http://$(cat /tmp/hostname).local/api/v1/commands/?cmd=play
-		;;
-
-		[P])
-		curl http://$(cat /tmp/hostname).local/api/v1/commands/?cmd=pause 
-		;;
-
-		[s])
-		curl http://$(cat /tmp/hostname).local/api/v1/commands/?cmd=stop
-		;;
-	esac
+# "q"キーを入力で終了,それ以外で一覧に表示されたコマンドを入力で実行
+while :
+do
+	read -p "command? > " "command"
+		case "$command" in
+			# 再生
+			[p])
+			curl http://$(cat /tmp/hostname).local/api/v1/commands/?cmd=play
+			;;
+	
+			# 一時停止
+			[P])
+			curl http://$(cat /tmp/hostname).local/api/v1/commands/?cmd=pause
+			;;
+			
+			# 停止
+			[s])
+			curl http://$(cat /tmp/hostname).local/api/v1/commands/?cmd=stop
+			;;
+	
+			# 終了
+			[q])
+			exit 0
+			;;
+		esac
+done
