bc825803837f4757bd395e540bc8e9008c0666dc
tekkamelon <type2guntank@gmail.com>
2021-05-05 01:04:24 +0900

    軽微な修正
    playとpauseをtoggleに統合,previousとnextを実装

--- volumio_cli/volumio_cli.sh
+++ volumio_cli/volumio_cli.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-# ホスト名の設定
-if ! cat /tmp/hostname ; then
+# "/tmp/hostnam"eが無い場合にホスト名を設定
+if ! cat /tmp/hostname > /dev/null ; then
 	echo "http://<<hostname>>.local" && read -p "hostname? > " "hostname" ; echo "$hostname" > /tmp/hostname
 else
 	:
 fi
 
 # pingで疎通確認,成功時のみ入力を待つ
-if ping -c 3 $(cat /tmp/hostname).local | grep ttl ; then
+if ping -c 3 $(cat /tmp/hostname).local | grep ttl > /dev/null ; then
 	# プレイリスト一覧を表示,sedで改行,echoで空白行の挿入
 	curl http://$(cat /tmp/hostname).local/api/v1/listplaylists | sed 's/,/\n/g' && echo -e "\n" 
 
 # コマンド一覧
 echo "command list" 
-echo "	playback          -> [1]"
-echo "	pause             -> [2]"
-echo "	stop              -> [3]"
+echo "	play/pause        -> [1]"
+echo "	stop              -> [2]"
+echo "	previous          -> [3]"
+echo "	next              -> [4]"
 echo "	change host       -> [C]"
 echo "	exit              -> [Q]"
 echo -n -e "\n"
@@ -26,19 +27,24 @@ while :
 do
 	read -p "command? > " "command"
 		case "$command" in
-			# 再生
+			# 再生/一時停止
 			[1])
-				curl http://$(cat /tmp/hostname).local/api/v1/commands/?cmd=play && echo -n -e "\n"
+				curl http://$(cat /tmp/hostname).local/api/v1/commands/?cmd=toggle && echo -n -e "\n"
 			;;
 	
-			# 一時停止
-			[2])
-				curl http://$(cat /tmp/hostname).local/api/v1/commands/?cmd=pause && echo -n -e "\n"
-			;;
-			
 			# 停止
-			[3])
+			[2])
 				curl http://$(cat /tmp/hostname).local/api/v1/commands/?cmd=stop && echo -n -e "\n"
+			;;
+	
+			# 前の曲
+			[3])
+				curl http://$(cat /tmp/hostname).local/api/v1/commands/?cmd=prev && echo -n -e "\n"
+			;;
+	
+			# 次の曲
+			[4])
+				curl http://$(cat /tmp/hostname).local/api/v1/commands/?cmd=next && echo -n -e "\n"
 			;;
 	
 			# ホスト名の再設定
@@ -53,5 +59,6 @@ do
 		esac
 done
 else
+	# ホストが見つからない場合は"/tmp/hostname"を削除
 	rm /tmp/hostname
 fi
