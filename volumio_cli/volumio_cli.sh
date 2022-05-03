fc91cca83b8c73dbd03d868f8162e316ba2b0d57
tekkamelon <type2guntank@gmail.com>
2022-05-03 23:09:49 +0900

    軽微な修正
    変数名の変更,ステータス表示の変更

--- volumio_cli/volumio_cli.sh
+++ volumio_cli/volumio_cli.sh
@@ -2,30 +2,29 @@
 
 # キーボードの入力を読み取りホスト名またはIPアドレスを設定
 read_hostname () {
-	echo "http://<<hostname or IP_adress>>" && echo 'hostname > ' | tr "\n" " " && read hostname ; echo "$hostname" > /tmp/hostname && echo "" && export VOLUMIO_HOST=$(cat /tmp/hostname)
+	echo "http://<<hostname or IP_adress>>" && echo 'hostname > ' | tr "\n" " " && read hostname ; echo "$hostname" > /tmp/hostname && echo "" && export MPD_HOST=$(cat /tmp/hostname)
 }
 
 # "/tmp/hostname"が無い場合にホスト名を設定
 test -e /tmp/hostname || read_hostname
 
 # wgetで疎通確認,成功時のみ入力を待つ
-if export VOLUMIO_HOST=$(cat /tmp/hostname) && wget -q -O - $VOLUMIO_HOST > /dev/null ; then
+if export MPD_HOST=$(cat /tmp/hostname) && wget -q -O - $MPD_HOST > /dev/null ; then
 
 	# apiを叩く
 	hit_apt () {
-		wget -q -O - http://$VOLUMIO_HOST/api/v1/commands/?cmd=$1 > /dev/null ; echo ""
+		wget -q -O - http://$MPD_HOST/api/v1/commands/?cmd=$1 > /dev/null ; echo ""
 	}
 
 	# システム情報の表示,改行
 	sys_info () {
-		echo "" && wget -q -O - http://$VOLUMIO_HOST/api/v1/getSystemInfo | sed -e 's/,/\n/g' -e 's/{//g' | awk -F\" '{print $2,$3,$4}' && echo ""
+		wget -q -O - http://$MPD_HOST/api/v1/getSystemInfo | sed -e 's/,/\n/g' -e 's/{//g' -e 's/\"//g' | grep status | cut -d\: -f 2- && echo ""
 	}
 
 # コマンド一覧を表示
 commands_list () {
 	cat << EOS
 
-	$(mpc --host=$VOLUMIO_HOST version)
 	command list
 	  playlist        -> [0]
 	  play/pause      -> [1]
@@ -55,43 +54,48 @@ do
 
 	# プレイリスト一覧を表示,trで改行,cutで抽出,echoで空白行の挿入
 			[0])
-				echo "" && wget -q -O - http://$VOLUMIO_HOST/api/v1/listplaylists | tr "," "\n" | cut -d\" -f 2 && echo ""
+				echo "" && wget -q -O - http://$MPD_HOST/api/v1/listplaylists | tr "," "\n" | cut -d\" -f 2 && echo ""
 			;;
 	
 			# 再生/一時停止
 			[1])
-				hit_apt toggle && sys_info | grep status && echo ""
+				hit_apt toggle && sys_info
 
 			;;
 	
 			# 停止
 			[2])
-				hit_apt stop
+				hit_apt stop && sys_info
+
 			;;
 	
 			# 前の曲
 			[3])
-				hit_apt prev
+				hit_apt prev && sys_info
+
 			;;
 	
 			# 次の曲
 			[4])
-				hit_apt next
+				hit_apt next && sys_info
+
 			;;
 
 			# リピート 
 			[5])
-				hit_apt repeat
+				hit_apt repeat && sys_info
+
 			;;
 
 			# ランダム
 			[6])
-				hit_apt random
+				hit_apt random && sys_info
+
 			;;
 
 			# システム情報の表示
 			[7])
-				sys_info		
+				echo "" && wget -q -O - http://$MPD_HOST/api/v1/getSystemInfo | sed -e 's/,/\n/g' -e 's/{//g' -e 's/\}//g' -e 's/\"//g'  | cut -d\: -f 2- && echo ""
 			;;
 
 			# 音量の調整
@@ -99,7 +103,7 @@ do
 				echo "" && echo 'volume? >' | tr "\n" " " && read volume
 
 				# echoでシングルクォート付きのURLをxargs経由でwgetに渡し,volumeを表示
-				echo "" && echo " 'http://$VOLUMIO_HOST/api/v1/commands/?cmd=volume&volume=$volume'" | xargs wget -q -O - > /dev/null ; sys_info | grep volume && echo "" 
+				echo "" && echo " 'http://$MPD_HOST/api/v1/commands/?cmd=volume&volume=$volume'" | xargs wget -q -O - > /dev/null ; sys_info | grep volume && echo "" 
 			;;
 
 			[H])
